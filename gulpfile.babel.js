ef3a32901c66fa57eb58fae16daf329cc731f365
ilhoon <rival6821@naver.com>
2019-07-11 21:55:59 +0900

    pug, del 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,24 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
+import del from "del";
 
-export const dev = () => console.log("hahaha");
+const routes = {
+  pug: {
+    src: "src/*.pug",
+    desc: "build"
+  }
+};
+
+const pug = () =>
+  gulp
+    .src(routes.pug.src)
+    .pipe(gpug())
+    .pipe(gulp.dest(routes.pug.desc));
+
+const clean = () => del(["build"]);
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+export const dev = gulp.series([prepare, assets]);
